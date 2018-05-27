274c40e212b9550cb32fca1c52039a4c4f8d268b
David Lopez Rodriguez <dloprodu@gmail.com>
2018-05-27 16:05:43 +0100

    DishDetailActivity, DishFormActivity and DishListActivity

---
+++ app/src/main/java/io/keepcoding/smartwaiter/activity/DishDetailActivity.kt
@@ -0,0 +1,13 @@
+package io.keepcoding.smartwaiter.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.keepcoding.smartwaiter.R
+
+class DishDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dish_detail)
+    }
+}
